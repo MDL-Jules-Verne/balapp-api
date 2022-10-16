6a822ecc8c93d7951295ed36d77e1dd6bc35c528
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2022-10-16 15:42:32 +0200

    fix bug

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express()
 const mongoose = require('mongoose')
 const fs = require('fs')
 let files =  fs.readdirSync("routes")
+
+app.use(express.json());
+
 // activate all routes from /routes
 files.forEach((e,i)=>{
     app.use(`/${e.slice(0,e.length-3)}`, require(`./routes/${e}`))
 })
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 mongoose.connect(
     "mongodb://balapp:7fFeCDS3TlPSHDRn8yAk@45.135.56.131:8586/balapp?authSource=winhalla&readPreference=primary&directConnection=true&ssl=false",
     {useNewUrlParser: true, useUnifiedTopology: true},

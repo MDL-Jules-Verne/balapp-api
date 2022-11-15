1472acccd369ab16d50e244b60925cdff6d15a5a
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2022-11-15 23:04:30 +0100

    added ws

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const fs = require('fs')
+const ticket = require('./models/ticket')
 let files =  fs.readdirSync("routes")
-
+let server = require('http').createServer()
+let WSServer = require('ws').Server
 app.use(express.json());
 
 // activate all routes from /routes
 files.forEach((e,i)=>{
     app.use(`/${e.slice(0,e.length-3)}`, require(`./routes/${e}`))
 })
-//TODD: connect to a different database if it's the local server
+//TODD: connect to local database
 mongoose.connect(
     "mongodb://balapp:7fFeCDS3TlPSHDRn8yAk@45.135.56.131:8586/balapp?authSource=winhalla&readPreference=primary&directConnection=true&ssl=false",
     {useNewUrlParser: true, useUnifiedTopology: true},
@@ -22,5 +24,16 @@ mongoose.connect(
     }
 );
 //mongodb://balapp:7fFeCDS3TlPSHDRn8yAk@45.135.56.131:8586/balapp?authSource=winhalla&readPreference=primary&directConnection=true&ssl=false
-
-app.listen(2000)
+let wss = new WSServer({
+    server: server
+})
+const mode = "registerTickets"
+server.on("request", app)
+wss.on("connection", (socket)=>{
+    console.log(`connected:`);
+    socket.on("message", async (message)=>{
+        console.log(`received: ${message}`);
+        socket.send(JSON.stringify({mode, db: await ticket.find()}))
+    })
+})
+server.listen(2000)
